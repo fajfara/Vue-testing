58a5d6fa2d79812b88adc3be656909e4870a65bd
Anže Fajfar <a.fajfar95@gmail.com>
2018-06-11 19:45:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <app-header></app-header>
+        <app-ninjas></app-ninjas>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from './components/header.vue';
+import Footer from './components/footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default {
+    components:{
+        'app-header': Header,
+        'app-footer': Footer,
+        'app-ninjas': Ninjas
+    },
+    data () {
+        return {
+          
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
